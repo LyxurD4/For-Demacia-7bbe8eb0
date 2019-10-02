f6e64321a836b6becfcdcb604249340966fc33ad
MAURITS DOES <2111633@talnet.nl>
2019-10-02 13:21:56 +0200

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,14 @@
+<?php
+$klas = array(
+    "TheLegend27",
+    "NoobMaster69",
+    "MLG_Pro_Quakecrafter_HHHD",
+    "XxX_Pro_PVP_YT",
+    "XxX_drop_awp_69_XxX",
+);
+
+for ($i = 0; $i <= count ($klas); $i++) {
+    echo $klas[$i]. PHP_EOL;
+}
+
+?>
